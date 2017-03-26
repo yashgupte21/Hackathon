92a54fb95ec1b3d6a1bfe2b386809724859cb486
ysahgupte21 <ysahgupte21@gmail.com>
2017-03-26 11:56:35 +0530

    commit14

---
+++ home.php
@@ -0,0 +1,118 @@
+<?php
+
+
+
+
+
+
+
+
+ ?>
+
+
+
+
+<html>
+<head>
+<title>Home
+</title>
+<style>
+#title{
+	width:80%;
+	height:6%;
+	border-radius: 0px 0px 50px 50px;
+	background: #77b4d8;
+	box-shadow: 0px 9px 15px #969696;
+	color:white;
+
+}
+td{
+	width:10%;
+	height:10%;
+	border-radius: 200px 200px 200px 200px;
+	background: #77b4d8;
+	box-shadow: 0px 8px 18px #969696;
+	text-align:center;
+	font-weight:bold;
+	font-size:20;
+}
+h1{
+    text-shadow: 0px 0px 10px black;
+}
+td{
+	color:white;
+    text-shadow: 0px 0px 10px black;
+}
+ul {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+    background-color: #333;
+	box-shadow: 0px 9px 15px #969696;
+}
+
+li {
+    float: left;
+	font-weight:light;
+	font-size:18;
+	font-family:arial;
+}
+
+li a {
+    display: block;
+    color: white;
+    text-align: center;
+    padding: 14px 16px;
+    text-decoration: none;
+}
+
+li a:hover:not(.active) {
+    background-color: #111;
+}
+
+.active {
+    background-color: #77b4d8;
+}
+button[type=button] {
+    width: 50%;
+    background-color: green;
+    color: white;
+    padding: 14px 20px;
+    margin: 8px 0;
+    border: none;
+    border-radius: 100px;
+    cursor: pointer;
+}
+
+a {
+	color : white;
+	text-decoration: none;
+
+}
+</style>
+</head>
+<body>
+<center>
+<div id="title"><h1 style="text-align:center">Don Bosco Institute of Technology, Kurla (W)</h1></div>
+<br>
+<ul>
+  <li><a class="active" href="http://localhost/hackathon/home.php">Home</a></li>
+  <li><a href="http://localhost/hackathon/signup.php">Sign Up</a></li>
+  <li><a href="http://localhost/hackathon/form2.php">Log In</a></li>
+</ul>
+<table cellpadding="50" cellspacing="60">
+<tr>
+<td>Git<br><sub><br>On 29/03/17 at 5 p.m.</sub><button type="button" value="" style="float:right"><a href="register.html">Register</a></td>
+<td>Ubuntu and Linux commandline<br><sub><br>On 9/04/17 at  9 a.m.</sub><button type="button" value="" style="float:right"><a href="register.html">Register</td>
+<td>Python<br><sub><br>On 15/04/17 at 5 p.m.</sub><button type="button" value="" style="float:right"><a href="register.html">Register</td>
+</tr>
+<tr>
+<td>Android<br><sub><br>On 17/04/17 at 2 p.m.</sub><button type="button" value="" style="float:right"><a href="register.html">Register</td>
+<td>Web Tech<br><sub><br>On 19/04/17 at 3 p.m.</sub><button type="button" value="" style="float:right"><a href="register.html">Register</td>
+<td>Database Basics<br><sub><br>On 24/04/17 at 5 p.m.</sub><button type="button" value="" style="float:right"><a href="register.html">Register</td>
+</tr>
+<table>
+</center>
+</body>
+</html>
